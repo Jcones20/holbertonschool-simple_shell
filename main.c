70dbfcd459c2e0d9e12bae31cfba68d83f5412d0
root <duewamgordon@gmail.com>
2023-08-04 13:22:27 -0700

    simpleshell2

--- main.c
+++ main.c
@@ -9,14 +9,14 @@ int main(int ac, char **argv)
 {
 	char *prompt = "holberton shell $";
 	char *lineptr;
-	size_t n =0;
+	size_t n = 0;
 
 	(void)ac;
 	(void)argv;
 
 	printf("%s", prompt);
 	getline(&lineptr, &n, stdin);
-	printf("%s", prompt);
+	printf("%s\n", prompt);
 
 	free(lineptr);
 	return (0);

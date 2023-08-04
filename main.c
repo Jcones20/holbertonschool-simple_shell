6e6af276c4d954645254706ded63d9fddaeb508a
root <duewamgordon@gmail.com>
2023-08-04 13:03:54 -0700

    simpleshell

---
+++ main.c
@@ -0,0 +1,18 @@
+#include "header.h"
+
+int main(int ac, char **argv)
+{
+	char *prompt = "holberton shell $";
+	char *lineptr;
+	size_t n =0;
+
+	(void)ac;
+	(void)argv;
+
+	printf("%s", prompt);
+	getline(&lineptr, &n, stdin);
+	printf("%s", prompt);
+
+	free(lineptr);
+	return (0);
+}
